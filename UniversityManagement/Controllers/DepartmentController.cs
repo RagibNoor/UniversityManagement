fd85dccec072eb9649e16918ec7ad9a178d8c73b
RagibNoor <ragibnoor19@gmail.com>
2018-09-17 01:03:13 +0600

    up to case 3 Full Complete(2)

--- UniversityManagement/Controllers/DepartmentController.cs
+++ UniversityManagement/Controllers/DepartmentController.cs
@@ -8,46 +8,46 @@ using UniversityManagement.Models;
 
 namespace UniversityManagement.Controllers
 {
-    public class DepartmentController : Controller
-    {
-        DepartmentBLL departmentBll = new DepartmentBLL();
-        //
-        // GET: /Department/
-        public ActionResult Index()
-        {
-            return View("Save");
-        }
-        [HttpGet]
-        public ActionResult Save()
-        {
-            return View();
-        }
+	public class DepartmentController : Controller
+	{
+		DepartmentBLL departmentBll = new DepartmentBLL();
+		//
+		// GET: /Department/
+		public ActionResult Index()
+		{
+			return View("Save");
+		}
+		[HttpGet]
+		public ActionResult Save()
+		{
+			return View();
+		}
 
-        [HttpPost]
-        public ActionResult Save(Department department)
-        {
-            ViewBag.Message = departmentBll.Save(department);
-            return View();
-        }
+		[HttpPost]
+		public ActionResult Save(Department department)
+		{
+			ViewBag.Message = departmentBll.Save(department);
+			return View();
+		}
 
-        public ActionResult ViewAllDepartment()
-        {
-            List<Department> departments = departmentBll.GetDepartments();
-            return View(departments);
-        }
-        public JsonResult IsCodeExist(string code)
-        {
-            List<Department> departments = departmentBll.GetDepartments();
-            bool isExist = departments.FirstOrDefault(u => u.Code.ToLowerInvariant().Equals(code.ToLower())) != null;
-            return Json(!isExist, JsonRequestBehavior.AllowGet);
-        }
+		public ActionResult ViewAllDepartment()
+		{
+			List<Department> departments = departmentBll.GetDepartments();
+			return View(departments);
+		}
+		public JsonResult IsCodeExist(string code)
+		{
+			List<Department> departments = departmentBll.GetDepartments();
+			bool isExist = departments.FirstOrDefault(u => u.Code.ToLowerInvariant().Equals(code.ToLower())) != null;
+			return Json(!isExist, JsonRequestBehavior.AllowGet);
+		}
 
-        [HttpGet]
-        public JsonResult IsNameExist(string Name)
-        {
-            List<Department> departments = departmentBll.GetDepartments();
-            bool isExist = departments.FirstOrDefault(u => u.Name.ToLowerInvariant().Equals(Name.ToLower())) != null;
-            return Json(!isExist, JsonRequestBehavior.AllowGet);
-        }  
+		[HttpGet]
+		public JsonResult IsNameExist(string Name)
+		{
+			List<Department> departments = departmentBll.GetDepartments();
+			bool isExist = departments.FirstOrDefault(u => u.Name.ToLowerInvariant().Equals(Name.ToLower())) != null;
+			return Json(!isExist, JsonRequestBehavior.AllowGet);
+		}  
 	}
 }
